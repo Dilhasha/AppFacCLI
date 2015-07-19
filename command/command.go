3a89e8cffc1bc72ab97b9ff02141f990044e2c3c
dilhasha <dilhasha.uom@gmail.com>
2015-07-19 23:40:29 +0530

    Configs func changed

--- command/command.go
+++ command/command.go
@@ -1,11 +1,14 @@
 package command
 
+
 type Command interface {
 	Metadata() CommandMetadata
-	Configs(reqs CommandRequirements) CommandConfigs
+	//Configs(reqs CommandRequirements) CommandConfigs
+	Configs() CommandConfigs
 	Requirements() CommandRequirements
 	Run(c CommandConfigs)
 }
+
 
 
 
