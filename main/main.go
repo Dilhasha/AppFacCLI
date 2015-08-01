80fb6663c536f3ec35b5e57980ac67d86d3da806
dilhasha <dilhasha.uom@gmail.com>
2015-08-01 23:40:05 +0530

    Main function changed to check flags and args

--- main/main.go
+++ main/main.go
@@ -4,10 +4,14 @@ import (
 	"os"
 	"github.com/Dilhasha/AppFacCLI/cli/command"
 	"github.com/codegangsta/cli"
-	"strings"
-	"strconv"
+	"github.com/Dilhasha/AppFacCLI/cli/password"
 	"fmt"
+
 )
+
+type Requirement struct {
+	name string
+}
 
 func main() {
 	app := cli.NewApp()
@@ -26,70 +30,75 @@ func main() {
 		app.Run(os.Args)
 	}else if _, ok := cmdFactory.CmdsByName[os.Args[1]]; ok {
 		c := cmdFactory.CmdsByName[os.Args[1]]
-		requirements:=c.Requirements(os.Args[2:])
-		configs:=c.Configs(requirements)
-		//configs := c.Configs(c.Metadata().Flags)
-		c.Run(configs);
+		cmdFlags:=cmdFactory.GetCommandFlags(c)
+		flagVals:=getRequirements(c,cmdFlags)
+		configs:=cmdFactory.GetCommandConfigs(c,flagVals)
+		c.Run(configs)
+		
+		
+
 	}else{
-		println("Command doe not exist")
+		println("Command does not exist")
 	}
 
 }
-func matchArgAndFlags(flags []string, args []string) string {
-	var badFlag string
-	var lastPassed bool
-	multipleFlagErr := false
+func getRequirements(c command.Command,cmdFlags []string)([]string){
 
-Loop:
-for _, arg := range args {
-	prefix := ""
-
-	//only take flag name, ignore value after '='
-	arg = strings.Split(arg, "=")[0]
-
-	if arg == "--h" || arg == "-h" {
-		continue Loop
-	}
-
-	if strings.HasPrefix(arg, "--") {
-		prefix = "--"
-	} else if strings.HasPrefix(arg, "-") {
-		prefix = "-"
-	}
-	arg = strings.TrimLeft(arg, prefix)
-
-	//skip verification for negative integers, e.g. -i -10
-	if lastPassed {
-		lastPassed = false
-		if _, err := strconv.ParseInt(arg, 10, 32); err == nil {
-			continue Loop
-		}
-	}
-
-	if prefix != "" {
-		for _, flag := range flags {
-			if flag == arg {
-				lastPassed = true
-				continue Loop
+	
+	var i=0
+	if(c.Metadata().SkipFlagParsing){
+		flags:=c.Metadata().Flags
+		reqs:= make([]string,len(flags),len(flags))
+		for n := 0; n < len(flags); n++ {
+			if flag, ok := flags[n].(cli.StringFlag); ok {
+				if (flag.Usage != "password") {
+					println(flag.Usage + " > ")
+					fmt.Scanf("%s", &reqs[i])
+					i++
+				}else {
+					reqs[i] = password.AskForPassword("Password")
+					i++
+				}
 			}
 		}
-
-		if badFlag == "" {
-			badFlag = fmt.Sprintf("\"%s%s\"", prefix, arg)
-		} else {
-			multipleFlagErr = true
-			badFlag = badFlag+fmt.Sprintf(", \"%s%s\"", prefix, arg)
+		return reqs
+	}else{
+		isMatch,flagVals:=matchArgAndFlags(cmdFlags,os.Args[2:])
+		
+		if(isMatch){
+			return flagVals
+		}else{
+			return nil
 		}
 	}
+	
+}
+func matchArgAndFlags(flags []string, args []string) (bool,[]string) {
+	var i=0
+	var reqs=make([]string,len(flags),len(flags))
+
+	Loop:
+	for _, flag := range flags {
+		containsFlag,index:=checkIfArgsContainsFlag(flag,args)
+		if (containsFlag){
+			reqs[i]=args[index]
+			i++
+			continue Loop
+		}else{
+			return false,reqs
+		}
+	}
+	return true,reqs
 }
 
-	if multipleFlagErr && badFlag != "" {
-		badFlag = fmt.Sprintf("%s %s", "Unknown flags:", badFlag)
-	} else if badFlag != "" {
-		badFlag = fmt.Sprintf("%s %s", "Unknown flag", badFlag)
-	}
+func checkIfArgsContainsFlag(flag string, args []string) (bool,int){
+	for n := 0; n < len(args); n++ {
+		if(args[n]==flag){
 
-	return badFlag
+			return true,n+1
+		}
+	}
+	return false,-1
 }
 
 
